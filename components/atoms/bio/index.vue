68603b013c199149fcbd738e86d5c8c2cb77f794
lermanori <lermanori@gmail.com>
2020-04-23 16:06:11 +0300

    op

--- components/atoms/bio/index.vue
+++ components/atoms/bio/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    :class="['text-green-400',home == true ?'text-3xl':'text-2xl',home == true ?'lg:text-5xl':'lg:text-4xl','text-center','font-agency']"
+    :class="['text-green-400',home == true ?'text-3xl':'text-2xl',home == true ?'lg:text-5xl':'lg:text-4xl','text-center','font-agency','p-1','px-6','md:mx-4']"
     :style="{textAlign:'center'}"
     v-html="bio"
   ></div>
